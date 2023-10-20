f51f2006face946d89fe05cfffa27b87be9f9e92
Nikolay Valaev <60339451+niklada9@users.noreply.github.com>
2023-10-20 17:26:06 +0300

    Create replace_spaces_with_underscores.py
    This script will recursively go through all subdirectories and replace spaces with underscores in both folder and file names.
    
    Replace "/path/to/your/folder" with the actual path to the folder where you want to replace spaces with underscores in file and folder names.

---
+++ replace_spaces_with_underscores.py
@@ -0,0 +1,31 @@
+# Replace "/path/to/your/folder" with the actual path to the folder where you want to replace spaces with underscores in file and folder names. 
+# This script will recursively go through all subdirectories and replace spaces with underscores in both folder and file names.
+
+
+import os
+
+def replace_spaces_with_underscores(folder_path):
+    for root, dirs, files in os.walk(folder_path):
+        # Rename folders
+        for dir_name in dirs:
+            original_dir_path = os.path.join(root, dir_name)
+            new_dir_name = dir_name.replace(" ", "_")
+            new_dir_path = os.path.join(root, new_dir_name)
+
+            if original_dir_path != new_dir_path:
+                os.rename(original_dir_path, new_dir_path)
+                print(f"Renamed folder: {original_dir_path} -> {new_dir_path}")
+
+        # Rename files
+        for file_name in files:
+            original_file_path = os.path.join(root, file_name)
+            new_file_name = file_name.replace(" ", "_")
+            new_file_path = os.path.join(root, new_file_name)
+
+            if original_file_path != new_file_path:
+                os.rename(original_file_path, new_file_path)
+                print(f"Renamed file: {original_file_path} -> {new_file_path}")
+
+if __name__ == "__main__":
+    folder_path = "/path/to/your/folder"  # Change this to your folder path
+    replace_spaces_with_underscores(folder_path)
