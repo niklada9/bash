e03cd8c732e439d8844407d5ac65b872a5e7e853
Nikolay Valaev <60339451+niklada9@users.noreply.github.com>
2023-10-20 18:06:22 +0300

    Update replace_spaces_with_underscores.py
    avoid conflicts with folder names

--- replace_spaces_with_underscores.py
+++ replace_spaces_with_underscores.py
@@ -3,6 +3,7 @@
 
 
 import os
+import shutil
 
 def replace_spaces_with_underscores(folder_path):
     for root, dirs, files in os.walk(folder_path):
@@ -21,8 +22,19 @@ def replace_spaces_with_underscores(folder_path):
                 counter += 1
 
             if original_dir_path != new_dir_path:
-                os.rename(original_dir_path, new_dir_path)
-                print(f"Renamed folder: {original_dir_path} -> {new_dir_path}")
+                try:
+                    os.rename(original_dir_path, new_dir_path)
+                    print(f"Renamed folder: {original_dir_path} -> {new_dir_path}")
+                except OSError:
+                    # If renaming fails, merge the contents of both directories
+                    for item in os.listdir(original_dir_path):
+                        src = os.path.join(original_dir_path, item)
+                        dst = os.path.join(new_dir_path, item)
+                        if os.path.isdir(src):
+                            shutil.move(src, dst)
+                        else:
+                            shutil.copy2(src, dst)
+                    shutil.rmtree(original_dir_path)
 
         # Rename files
         for file_name in files:
@@ -30,7 +42,7 @@ def replace_spaces_with_underscores(folder_path):
             new_file_name = file_name.replace(" ", "_")
             new_file_path = os.path.join(root, new_file_name)
 
-            # Check for existing file with the same name
+            # Check for an existing file with the same name
             counter = 2
             while os.path.exists(new_file_path):
                 base_name, extension = os.path.splitext(new_file_name)
@@ -43,7 +55,8 @@ def replace_spaces_with_underscores(folder_path):
                 print(f"Renamed file: {original_file_path} -> {new_file_path}")
 
 if __name__ == "__main__":
-    folder_path = "/media/nikmin/arc/ya_disk/upo"  # Change this to your folder path
+    folder_path = "/path/to/your/folder"  # Change this to your folder path
     replace_spaces_with_underscores(folder_path)
+
 
  # OSError: [Errno 39] Directory not empty: '/media/nikmin/arc/ya_disk/upo/2007_05_07 Piter_JJ' -> '/media/nikmin/arc/ya_disk/upo/2007_05_07_Piter_JJ'
